44a50b16c00344c11e5b5ca3449cba33c285377f
Laboratório-00-00 <Laboratrio-00-00@192.168.231.51>
2019-02-26 20:04:35 -0300

    Projeto Criado

--- TesteJava/src/testejava/TesteJava.java
+++ TesteJava/src/testejava/TesteJava.java
@@ -15,7 +15,6 @@ public class TesteJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("opa néne");
     }
-    
 }
