ef0c2afec88ec4691c582de7b0643f5f7bf13231
Laboratório-00-00 <Laboratrio-00-00@192.168.231.51>
2019-02-26 20:06:42 -0300

    Revert "Projeto Criado"
    This reverts commit 44a50b16c00344c11e5b5ca3449cba33c285377f.

--- TesteJava/src/testejava/TesteJava.java
+++ TesteJava/src/testejava/TesteJava.java
@@ -15,6 +15,7 @@ public class TesteJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("opa néne");
+        // TODO code application logic here
     }
+    
 }
